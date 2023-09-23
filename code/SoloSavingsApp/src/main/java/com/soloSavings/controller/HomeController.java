f9d81a349f3fbd6bab3dd994dc7245078fd81849
Hanwen Zhang <hanwenzhang888@gmail.com>
2023-09-23 01:08:24 -0400

    Home controller refactory

--- code/SoloSavingsApp/src/main/java/com/soloSavings/controller/HomeController.java
+++ code/SoloSavingsApp/src/main/java/com/soloSavings/controller/HomeController.java
@@ -11,6 +11,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.web.servlet.error.ErrorController;
 
 @Controller
 public class HomeController {
@@ -28,4 +30,20 @@ public class HomeController {
         return "home";
     }
 
+    @GetMapping("/solosavings/register")
+    public String register() {
+        logger.info(" Register an account with SoloSavings Application");
+        return "register";
+    }
+    @GetMapping("/solosavings/login")
+    public String login() {
+        logger.info(" Login to SoloSavings Application");
+        return "login";
+    }
+
+    @RequestMapping("/error")
+    public String handleError() {
+        // Handle errors and return the appropriate template
+        return "error"; // For error JSP page
+    }
 }
--- code/SoloSavingsApp/src/main/java/com/soloSavings/controller/UserAuthenticationController.java
+++ code/SoloSavingsApp/src/main/java/com/soloSavings/controller/UserAuthenticationController.java
@@ -2,8 +2,8 @@ package com.soloSavings.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /*
  * Copyright (c) 2023 Team 2 - SoloSavings
@@ -13,17 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
  * This software is the confidential and proprietary information of
  * Team 2 - SoloSavings Application
  */
-@Controller
+
+@RestController
+@RequestMapping("/api")
 public class UserAuthenticationController {
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-    @GetMapping("/solosavings/register")
-    public String register() {
-        logger.info(" Register an account with SoloSavings Application");
-        return "register";
-    }
-    @GetMapping("/solosavings/login")
-    public String login() {
-        logger.info(" Login to SoloSavings Application");
-        return "login";
-    }
+
 }
