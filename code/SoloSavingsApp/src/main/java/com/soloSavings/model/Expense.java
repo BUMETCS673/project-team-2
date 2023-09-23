977c94ffc3960f29008493adbe2013d7e27dbea8
Christan Pratt <clpratt@bu.edu>
2023-09-22 20:39:30 -0400

    Add Models

--- code/SoloSavingsApp/src/main/java/com/soloSavings/model/Expense.java
+++ code/SoloSavingsApp/src/main/java/com/soloSavings/model/Expense.java
@@ -1,4 +1,11 @@
 package com.soloSavings.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+
 /*
  * Copyright (c) 2023 Team 2 - SoloSavings
  * Boston University MET CS 673 - Software Engineering
@@ -7,5 +14,69 @@ package com.soloSavings.model;
  * This software is the confidential and proprietary information of
  * Team 2 - SoloSavings Application
  */
+@Entity
+@Table(name = "expenses")
 public class Expense {
+
+    @Id
+    @GeneratedValue
+    private int expense_id;
+    private int user_id;
+    private String category;
+    private Long amount;
+    private LocalDate expense_date;
+
+    public Expense(int expense_id, int user_id, String category, Long amount, LocalDate expense_date) {
+        this.expense_id = expense_id;
+        this.user_id = user_id;
+        this.category = category;
+        this.amount = amount;
+        this.expense_date = expense_date;
+    }
+
+    public Expense() {
+
+    }
+
+    public int getExpense_id() {
+        return expense_id;
+    }
+
+    public void setExpense_id(int expense_id) {
+        this.expense_id = expense_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getExpense_date() {
+        return expense_date;
+    }
+
+    public void setExpense_date(LocalDate expense_date) {
+        this.expense_date = expense_date;
+    }
+
 }
+
--- code/SoloSavingsApp/src/main/java/com/soloSavings/model/Income.java
+++ code/SoloSavingsApp/src/main/java/com/soloSavings/model/Income.java
@@ -1,4 +1,11 @@
 package com.soloSavings.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+
 /*
  * Copyright (c) 2023 Team 2 - SoloSavings
  * Boston University MET CS 673 - Software Engineering
@@ -7,5 +14,70 @@ package com.soloSavings.model;
  * This software is the confidential and proprietary information of
  * Team 2 - SoloSavings Application
  */
+@Entity
+@Table(name = "income")
 public class Income {
+
+    @Id
+    @GeneratedValue
+    private int income_id;
+    private int user_id;
+    private String source;
+    private Long amount;
+    private LocalDate income_date;
+
+    public Income(int income_id, int user_id, String source, Long amount, LocalDate income_date) {
+
+        this.income_id = income_id;
+        this.user_id = user_id;
+        this.source = source;
+        this.amount = amount;
+        this.income_date = income_date;
+    }
+
+    public Income() {
+
+    }
+
+    public int getIncome_id() {
+        return income_id;
+    }
+
+    public void setIncome_id(int income_id) {
+        this.income_id = income_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getIncome_date() {
+        return income_date;
+    }
+
+    public void setIncome_date(LocalDate income_date) {
+        this.income_date = income_date;
+    }
+
 }
+
