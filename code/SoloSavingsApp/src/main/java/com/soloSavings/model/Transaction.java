bab62d5855a14695e657fdd6564a6a8c1c6ff3eb
to-whitney <nguyetto@bu.edu>
2023-09-24 12:14:46 -0700

    added enum as type so jpa maps correctly to db

--- code/SoloSavingsApp/src/main/java/com/soloSavings/model/Transaction.java
+++ code/SoloSavingsApp/src/main/java/com/soloSavings/model/Transaction.java
@@ -2,10 +2,7 @@ package com.soloSavings.model;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.soloSavings.model.helper.TransactionType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,6 +24,7 @@ public class Transaction {
     private Integer transaction_id;
     private Integer user_id;
     private String source;
+    @Enumerated(EnumType.STRING)
     private TransactionType transaction_type;
     private Double amount;
     private LocalDate transaction_date;
