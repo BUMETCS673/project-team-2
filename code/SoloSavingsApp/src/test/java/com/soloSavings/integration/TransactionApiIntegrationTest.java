bf3b6c27eb4a6a80d9fef35e8e89f407e3779b40
to-whitney <nguyetto@bu.edu>
2023-10-03 00:00:51 -0700

    remove depend. not used

--- code/SoloSavingsApp/src/test/java/com/soloSavings/integration/TransactionApiIntegrationTest.java
+++ code/SoloSavingsApp/src/test/java/com/soloSavings/integration/TransactionApiIntegrationTest.java
@@ -21,7 +21,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase
-@Profile("test")
 public class TransactionApiIntegrationTest {
 
     @Autowired
