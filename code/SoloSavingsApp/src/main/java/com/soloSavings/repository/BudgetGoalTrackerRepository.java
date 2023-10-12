50ca6b772981d4dd4fc3daa91825246f6147833b
Andrew Ouellette <AndrewOuellette1993@gmail.com>
2023-10-11 21:45:01 -0400

    Get actualAmount to populate

--- code/SoloSavingsApp/src/main/java/com/soloSavings/repository/BudgetGoalTrackerRepository.java
+++ code/SoloSavingsApp/src/main/java/com/soloSavings/repository/BudgetGoalTrackerRepository.java
@@ -1,15 +1,16 @@
 package com.soloSavings.repository;
 
 import com.soloSavings.model.BudgetGoalTracker;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface BudgetGoalTrackerRepository {
+public interface BudgetGoalTrackerRepository extends JpaRepository<BudgetGoalTracker, Integer> {
 
-    @Query("SELECT t FROM BudgetGoalTracker t WHERE t.user_id = ?1")
+    @Query("SELECT t FROM BudgetGoalTracker t WHERE t.userId = ?1")
     List<BudgetGoalTracker> findAllGoalsByUserId(Integer userId);
 
 }
