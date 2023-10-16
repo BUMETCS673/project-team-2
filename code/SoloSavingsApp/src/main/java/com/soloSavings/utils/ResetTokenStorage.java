1ae685b384759defdf4dd8646626c015eed2d1f7
to-whitney <nguyetto@bu.edu>
2023-10-15 18:27:26 -0700

    coverage of about 80% now

--- code/SoloSavingsApp/src/main/java/com/soloSavings/utils/ResetTokenStorage.java
+++ code/SoloSavingsApp/src/main/java/com/soloSavings/utils/ResetTokenStorage.java
@@ -26,13 +26,4 @@ public class ResetTokenStorage {
     public void removeTokenRecord(String userName) {
         tokenMap.remove(userName);
     }
-
-    public void printTokenMap() {
-        System.out.println("Token Map Contents:");
-        for (Map.Entry<String, String> entry : tokenMap.entrySet()) {
-            String username = entry.getKey();
-            String token = entry.getValue();
-            System.out.println("Username: " + username + ", Token: " + token);
-        }
-    }
 }
